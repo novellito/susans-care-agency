16ffc8a21b6b4827671496431c70da354fff9718
Sonny Trinidad <strinidad70@gmail.com>
2017-06-16 15:48:44 -0700

    Fixed header.php

--- components/navbar.php
+++ components/navbar.php
@@ -9,14 +9,14 @@
         <a class="navbar-brand" href="index.php">
             <img src="assets/images/susanLogo.png" alt="Logo">
             <span class="subHead h3">Susan's Care Agency</span>
-            <p>Loving Caring People</p>
+            <p>Loving &amp; Caring People</p>
         </a>
 
         <div class="collapse navbar-collapse" id="menuCollapse">
             <div class="navbar-nav ml-4">
                 <a class="nav-item nav-link active" href="index.php">Home</a>
                 <a class="nav-item nav-link" href="staff.php">Our Staff</a>
-                <a class="nav-item nav-link" href="#">Testimonials</a>
+<!--                <a class="nav-item nav-link" href="#">Testimonials</a>-->
                 <a class="nav-item nav-link" href="about.php">About Us</a>
             </div>
         </div>
