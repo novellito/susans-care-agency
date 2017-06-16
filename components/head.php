0d20ad7b98664b6814387db2b962d53d5493d9fd
Sonny Trinidad <strinidad70@gmail.com>
2017-06-16 15:19:13 -0700

    Synchonized files head.php navbar.php style.css against index.html and style.css

--- components/head.php
+++ components/head.php
@@ -6,6 +6,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
     <!--<link rel="stylesheet" href="bootstrap/bootstrap.min.css">-->
-    <link href="https://fonts.googleapis.com/css?family=Lobster|Ubuntu:400i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Ubuntu:400" rel="stylesheet">
     <title>Susan's Care Agency</title>
 </head>
--- components/navbar.php
+++ components/navbar.php
@@ -1,14 +1,25 @@
-   <nav class="navbar navbar-toggleable navbar-inverse hidden-xs-down">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">
-                <img src="assets/images/dummyLogo.png" alt="Dummy Logo">
-                <span class="subHead h3">Susan's Care Agency</span>
-            </a>
-            <div class="navbar-nav">
+<nav class="navbar navbar-toggleable navbar-inverse hidden-xs-down">
+
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#menuCollapse" aria-controls="menuCollapse" aria-expanded="false" aria-label="Navigation Toggle">
+            <span class="navbar-toggler-icon"></span>
+        
+        </button>
+
+    <div class="container">
+        <a class="navbar-brand" href="index.php">
+            <img src="assets/images/susanLogo.png" alt="Logo">
+            <span class="subHead h3">Susan's Care Agency</span>
+            <p>Loving Caring People</p>
+        </a>
+
+        <div class="collapse navbar-collapse" id="menuCollapse">
+            <div class="navbar-nav ml-4">
                 <a class="nav-item nav-link active" href="index.php">Home</a>
                 <a class="nav-item nav-link" href="staff.php">Our Staff</a>
                 <a class="nav-item nav-link" href="#">Testimonials</a>
                 <a class="nav-item nav-link" href="about.php">About Us</a>
             </div>
         </div>
-    </nav>
+
+    </div>
+</nav>
