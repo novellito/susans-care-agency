80dd70df75b7f0a2138ba46c008bfef410b50ef2
Sonny Trinidad <strinidad70@gmail.com>
2017-06-17 09:17:04 -0700

    Add Contact Info in about.php

--- about.php
+++ about.php
@@ -14,13 +14,39 @@
         <p class="h1 mt-3">About Us</p>
         <img src="assets/images/12.jpg" class="mb-4" width="100%">
         
-        <div class="row">
-            <div class="col-sm-12">
-                <h2>Contact Us</h2>
-            </div>
+        <div class="row  justify-content-center">
+            <section class="col-sm-3">
+                <address>
+                   <span id="contact">Our Contact Info</span><br>
+                   <span id="susan">Susan Repollo</span><br>
+                    President<br><br>
+                    
+                    18100 Herbold Street<br>
+                    Northridge, CA 91325<br>
+                    818-830-9320<br>
+                    818-481-3945
+                 
+                
+                </address>
+
+            </section>
+            <section class="col-sm-3">
+                <address>
+                    <span id="susan">Letlet Something</span><br><br>
+                    628 Miner Terrace #2<br>
+                    Linden, NJ 07036<br>
+                    908-578-6821<br>
+                    
+                 
+                
+                </address>
+
+            </section>
         </div>
 
     </div>
+    
+    
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
